6d71f38bb2e54eb40f931bf550feb2264d6843e1
Bhupendra Kumar <bhupendrakumar1545@gmail.com>
2025-11-05 15:28:14 +0530

    New changes

--- Frontend/static/main.js
+++ Frontend/static/main.js
@@ -133,7 +133,7 @@ function updateGameUI(data) {
     if (data.status === 'won') {
         gameMessage.classList.add('success');
 
-        gameMessage.textContent = `🎉 ${data.hint}  ${data.target_number}! You found the number in ${data.attempts} attempts!`;
+        gameMessage.textContent = `🎉 ${data.hint}  ${data.target_number}! You found the correct number in ${data.attempts} attempts!`;
 
     } else if (data.status === 'quit') {
         gameMessage.classList.add('warning');
